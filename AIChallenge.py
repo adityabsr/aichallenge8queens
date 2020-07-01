6d69368083bf692b740d968eed5888dbcd1bd3ba
adityabsr <59690093+adityabsr@users.noreply.github.com>
2020-07-01 16:03:28 +0530

    Update AIChallenge.py

--- AIChallenge.py
+++ AIChallenge.py
@@ -1,127 +1,185 @@
-#!/usr/bin/env python
-# coding: utf-8
+import numpy as np
+import sys
 
-# The python code should have the following components
-# 
-# a. Code must follow Object Oriented program standards with appropriate Unit tests
-# 
-# b. Function to generate the initial population
-# 
-# c. Function to score the population
-# 
-# d. Function to do cross over and mutation of the selected gene pool
-# 
-# e. Main function to identify the right sequence
+nQueens = 8
+STOP_CTR = 28
+MUTATE = 0.000001
+MUTATE_FLAG = True
+MAX_ITER = 100000
+POPULATION = None
 
-# In[4]:
+class BoardPosition:
+	def __init__(self):
+		self.sequence = None
+		self.fitness = None
+		self.survival = None
+	def setSequence(self, val):
+		self.sequence = val
+	def setFitness(self, fitness):
+		self.fitness = fitness
+	def setSurvival(self, val):
+		self.survival = val
+	def getAttr(self):
+		return {'sequence':sequence, 'fitness':fitness, 'survival':survival}
 
+def fitness(chromosome = None):
+	"""
+	returns 28 - <number of conflicts>
+	to test for conflicts, we check for 
+	 -> row conflicts
+	 -> columnar conflicts
+	 -> diagonal conflicts
+	 
+	The ideal case can yield upton 28 arrangements of non attacking pairs.
+	for iteration 0 -> there are 7 non attacking queens
+	for iteration 1 -> there are 6 no attacking queens ..... and so on 
+	Therefore max fitness = 7 + 6+ 5+4 +3 +2 +1 = 28
+	hence fitness val returned will be 28 - <number of clashes>
+	"""
 
-# 8 Queens puzzle solutions using genetic algorithm
+	# calculate row and column clashes
+	# just subtract the unique length of array from total length of array
+	# [1,1,1,2,2,2] - [1,2] => 4 clashes
+	clashes = 0;
+	row_col_clashes = abs(len(chromosome) - len(np.unique(chromosome)))
+	clashes += row_col_clashes
 
-import random
-from scipy.special import comb
-from numpy import argmax
-
-def chromosome(minimum, maximum, chromosome_size):
-    
-    return [random.randint(minimum, maximum) for _ in range(chromosome_size)]
-
-
-def create_population(population_size, minimum,
-                      maximum, chromosome_size):
-    return [chromosome(minimum, maximum, chromosome_size)
-            for _ in range(population_size)]
-
-
-def fitness_fn(chromosome):
-    
-    checkmates = 0
-    pairs = list(zip(chromosome,
-                     [i+1 for i in range(len(chromosome))]))
-
-    for i in range(len(pairs)):
-        for j in range(i + 1, len(pairs)):
-            x, y = pairs[i]
-            w, z = pairs[j]
-            if x == w or y == z:
-                checkmates += 1
-            else:
-                dx = abs(x - w)
-                dy = abs(y - z)
-                if dx == dy:
-                    checkmates += 1
-    fitness = max_atacking_pairs - checkmates
-    return fitness
-
-def select_parents(population, probabilities, qtd_parents=2):
-        
-    parents = []
-    
-    for _ in range(qtd_parents):
-        draw = random.random()
-        for (i, parent) in enumerate(population):
-            if draw <= probabilities[i]:
-                parents.append(parent)
-                break
-    return tuple(parents)
-
-def reproduce(x, y):
-    
-    chromosome_size = len(x)
-    point = random.randint(1, chromosome_size-1)
-    return x[: point] + y[point :]
+	# calculate diagonal clashes
+	for i in range(len(chromosome)):
+		for j in range(len(chromosome)):
+			if ( i != j):
+				dx = abs(i-j)
+				dy = abs(chromosome[i] - chromosome[j])
+				if(dx == dy):
+					clashes += 1
 
 
-def mutate(chromosome):
-    
-    chromosome_size = len(chromosome)
-    point = random.randint(0, chromosome_size - 1)
-    new_value = random.randint(1, chromosome_size)
-    
-    while new_value == chromosome[point]:
-        new_value = random.randint(1, chromosome_size)
-    chromosome[point] = new_value
-    return chromosome
+	return 28 - clashes	
 
-def ga_eight_queens(population, fitness_fn, epochs=100):
-    
-    i = 0
-    solutions = []
-    
-    while i < epochs:    
-        new_population = []
-        individual_fitness = [fitness_fn(chromosome) for chromosome in population]
-        total_fitness = sum(individual_fitness)
-        percentage_fitness = [fitness/total_fitness for fitness in individual_fitness]
-        probabilities = [sum(percentage_fitness[:i+1]) for i in range(len(percentage_fitness))]
 
-        if max_atacking_pairs in individual_fitness:
-             
-            break
-        
-        for _ in range(len(population)):
-            x, y = select_parents(population, probabilities)
-            child = reproduce(x, y)
-            if random.random() <= mutation_probability:
-                child = mutate(child)
-            new_population.append(child)
-        population = new_population
-        i += 1
-    return (argmax([fitness_fn(e) for e in population]), population)
+def generateChromosome():
+	# randomly generates a sequence of board states.
+	global nQueens
+	init_distribution = np.arange(nQueens)
+	np.random.shuffle(init_distribution)
+	return init_distribution
 
-board_size = 8
-mutation_probability = 0.05
-population_size = 1000
-chromosome_size = board_size
-minimum = 1
-maximum = board_size
-epochs = 100
-max_atacking_pairs = int(comb(chromosome_size, 2))
-population = create_population(population_size, minimum, maximum, chromosome_size)
+def generatePopulation(population_size = 100):
+	global POPULATION
 
-index_best_individual, population = ga_eight_queens(population, fitness_fn, epochs)
-solutions = list(
-    filter(lambda individual: max_atacking_pairs == fitness_fn(individual),
-           population))
-print('solutions: ', solutions)
+	POPULATION = population_size
 
+	population = [BoardPosition() for i in range(population_size)]
+	for i in range(population_size):
+		population[i].setSequence(generateChromosome())
+		population[i].setFitness(fitness(population[i].sequence))
+
+	return population
+
+
+def getParent():
+	globals()	
+	parent1, parent2 = None, None
+	# parent is decided by random probability of survival.
+	# since the fitness of each board position is an integer >0, 
+	# we need to normaliza the fitness in order to find the solution
+	
+	summation_fitness = np.sum([x.fitness for x in population])
+	for each in population:
+		each.survival = each.fitness/(summation_fitness*1.0)
+
+	while True:
+		parent1_random = np.random.rand()
+		parent1_rn = [x for x in population if x.survival <= parent1_random]
+		try:
+			parent1 = parent1_rn[0]
+			break
+		except:
+			pass
+
+	while True:
+		parent2_random = np.random.rand()
+		parent2_rn = [x for x in population if x.survival <= parent2_random]
+		try:
+			t = np.random.randint(len(parent2_rn))
+			parent2 = parent2_rn[t]
+			if parent2 != parent1:
+				break
+			else:
+				print ("equal parents")
+				continue
+		except:
+			print ("exception")
+			continue
+
+	if parent1 is not None and parent2 is not None:
+		return parent1, parent2
+	else:
+		sys.exit(-1)
+
+def reproduce_crossover(parent1, parent2):
+	globals()
+	n = len(parent1.sequence)
+	c = np.random.randint(n, size=1)
+	child = BoardPosition()
+	child.sequence = []
+	child.sequence.extend(parent1.sequence[0:c])
+	child.sequence.extend(parent2.sequence[c:])
+	child.setFitness(fitness(child.sequence))
+	return child
+
+
+def mutate(child):
+	"""	
+	- according to genetic theory, a mutation will take place
+	when there is an anomaly during cross over state
+	- since a computer cannot determine such anomaly, we can define 
+	the probability of developing such a mutation 
+	"""
+	if child.survival < MUTATE:
+		c = np.random.randint(8)
+		child.sequence[c] = np.random.randint(8)
+	return child
+
+def GA(iteration):
+	
+	globals()
+	newpopulation = []
+	for i in range(len(population)):
+		parent1, parent2 = getParent()
+		# print "Parents generated : ", parent1, parent2
+
+		child = reproduce_crossover(parent1, parent2)
+
+		if(MUTATE_FLAG):
+			child = mutate(child)
+
+		newpopulation.append(child)
+	return newpopulation
+
+
+def stop():
+	globals()
+	
+	fitnessvals = [pos.fitness for pos in population]
+	if STOP_CTR in fitnessvals:
+		return True
+	if MAX_ITER == iteration:
+		return True
+	return False
+
+
+
+population = generatePopulation(1000)
+
+
+iteration = 0;
+while not stop():
+	# keep iterating till  you find the best position
+	population = GA(iteration)
+	iteration +=1 
+
+print ("Iteration number : ", iteration)
+for each in population:
+	if each.fitness == 28:
+		print (each.sequence)
